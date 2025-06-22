ddd2038ae9628fe26bb881d4411fe9213ff25be5
Dmytro Kosynskyy <dmytro.kosynskyy@stud.h-da.de>
2025-06-22 01:57:52 +0100

    ajax test

---
+++ js/project.js
@@ -0,0 +1,13 @@
+$(document).ready(function () {
+    $.ajax({
+        url: './assets/html/FYP.html',
+        method: 'GET',
+        dataType: 'html',
+        success: function (data) {
+            $('#projectContainer').html(data)
+        },
+        error: function (error) {
+            console.log('Error fetching data:', error);
+        }
+    });
+});
